e822297919d1f1d3440c18f9433b22e9df7403c2
Tanvir Khondakar <tanvirkhondakar@gmail.com>
2016-10-10 09:29:40 -0400

    Strings in Python

---
+++ strings.py
@@ -0,0 +1,37 @@
+date = "12/09/16"
+
+# Go through string and split
+# Where there is a '/'
+
+date_manip = date.split('/')
+
+# Show the outcome
+# print date_manip
+
+print 'Month: ' + date_manip[0]
+print 'Day: ' + date_manip[1]
+print 'Year: ' + date_manip[2]
+
+
+#Types of String Methods
+
+'''
+.upper()
+Changes string to uppercase
+
+.lower()
+Changes string to lowercase
+
+.capitalize()
+Capitalizes the first letter of the string
+
+.title()
+Capitalizes the first letter of every word in the string
+
+.swapcase()
+Switches the case of every character in the string
+
+.strip()
+Removes whitespace, tabs and indents from the string
+
+'''
