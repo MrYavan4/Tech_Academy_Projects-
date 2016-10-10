999824bc1c3cc9972df2df5125b14442c00269f2
Tanvir Khondakar <tanvirkhondakar@gmail.com>
2016-10-10 09:42:33 -0400

    Statements in Python

---
+++ statements.py
@@ -0,0 +1,49 @@
+# STATEMENTS IN PYTHON
+
+'''
+Several Statements:
+
+1. if, eles,elif (else if)
+
+2. for LOOP
+
+3. while LOOP
+
+'''
+
+'''
+
+Symbols:
+
+== Is equal to
+
+!= Is not equal to
+
+> Greater than
+
+< Less Then
+
+>= Greater than or equal to
+
+<= Less than or equal to
+
+'''
+
+# Define the variables
+
+x = 7
+
+# Start the statement
+
+if x == 10:
+    print 'x = 10'
+
+# If x does not equal 10, but equals 9 run this
+
+elif x == 9:
+    print 'x = 9'
+
+# If x is not equal to 10 or 9, run this
+
+else:
+    print 'x does not equal 9 or 10'
