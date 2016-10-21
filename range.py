d20b25b5dfc8c9baecf8dedeca0728ffbb2fa1ae
Tanvir Khondakar <tanvirkhondakar@gmail.com>
2016-10-21 11:30:13 -0400

    Range Function
    Working with the python range Function

---
+++ range.py
@@ -0,0 +1,20 @@
+# Python Range parameters
+
+my_list = [0,1,2,3]
+my_list_len = len(my_list)
+for i in range(0, my_list_len):
+    print(my_list[i])
+
+# Drill Question 1:
+
+for x in range(4):
+    print(x)
+
+# Drill Question 2:
+
+for x in range(3,-1,-1):
+    print(x)
+
+# Drill Question 3:
+for x in range(8,1,-2):
+    print(x)
