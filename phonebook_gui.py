30bd1636cdf22950bf0306eaae18e3a210f15d28
Tanvir Khondakar <tanvirkhondakar@gmail.com>
2016-11-16 12:38:47 -0500

    Changes Made

--- phonebook_gui.py
+++ phonebook_gui.py
@@ -53,14 +53,14 @@ def load_gui(self):
     self.scrollbar1 = Scrollbar(self.master, orient = VERTICAL)
     self.lstList1 = Listbox(self.master, exportselection = 0,
                             yscrollcommand = self.scrollbar1.set)
-    self.lstList1.bind('<<ListboxSelect>>', lambda event: phonebook_func.OnSelect(self,event))
+    self.lstList1.bind('<<ListboxSelect>>', lambda event: phonebook_func.onSelect(self,event))
     self.scrollbar1.config(command = self.lstList1.yview)
     self.scrollbar1.grid(row = 1, column = 5, rowspan = 7, columnspan = 1,
                          padx = (0,0), pady = (0,0), sticky = N+E+S)
     self.lstList1.grid(row = 1, column = 2, rowspan = 7, columnspan = 3,
                          padx = (0,0), pady = (0,0), sticky = N+E+S+W)
 
-    #Button
+    #Buttons
 
     self.btn_add = tk.Button(self.master, width = 12, height = 2, text = "Add",
                              command = lambda: phonebook_func.addToList(self))
@@ -76,7 +76,7 @@ def load_gui(self):
 
     self.btn_close = tk.Button(self.master, width = 12, height = 2, text = "Close",
                              command = lambda: phonebook_func.ask_quit(self))
-    self.btn_close.grid(row = 8, column = 0,columnspan = 1, padx = (15,0), pady = (45,10), sticky = E)
+    self.btn_close.grid(row = 8, column = 3,columnspan = 1, padx = (15,0), pady = (45,10), sticky = E)
     
 
     phonebook_func.create_db(self)
