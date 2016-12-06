1144045df3fcbe0f228238d99d94079960bb9c42
Tanvir Khondakar <tanvirkhondakar@gmail.com>
2016-12-06 15:18:23 -0500

    Add files via upload

--- Drill_C-_Part22/AccessModifiers.cs
+++ Drill_C-_Part22/AccessModifiers.cs
@@ -1,16 +1,103 @@
 ﻿using System;
 
 // Public Acess 
+
 class publicAcess
 {
     public int x = 10;
     public int y = 20;
 }
 
-class Main
+class MainClass
 {
     static void Main()
     {
-        Console.WriteLine("x = {} and y = {}", x, y);
+        publicAcess p = new text();
+        p.x = 10;
+        p.y = 20;
+        Console.WriteLine("x = {} and y = {}", p.x, p.y);
+    }
+}
+
+// Protected Access 
+
+// The protected keyword is a member access modifier. 
+// A protected member is accessible within its class and by derived class instances.
+
+class Protected
+{
+    protected int x = 10;
+    protected int y = 20;
+}
+
+class MainClass : Protected
+{
+    static void Main()
+    {
+        MainClass test = new MainClass();
+
+        // Direct access to protected members
+        test.x = 10;
+        test.y = 20;
+        Console.WriteLine("x = {} and y = {}", test.x, test.y);
+    }
+}
+
+// Internal: Only accessible within the same assembly 
+
+public class AssemblyClass
+{
+    internal static int x = 0;
+}
+
+public class TestAssembly
+{
+    static void Main()
+    {
+        // This will be OK
+        AssemblyClass test = new AssemblyClass();
+
+        // This will give us a CS0117 error
+        AssemblyClass.x = 222;
+    }
+}
+
+// Private
+// The private keyword is a member access modifier. 
+//Private access is the least permissive access level. 
+//Private members are accessible only within the body of the class or the struct in which they are declared
+
+class Employee2
+{
+    private string name = "FirstName, LastName";
+    private double salary = 100.0;
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public double Salary
+    {
+        get { return salary; }
+    }
+}
+
+class PrivateTest
+{
+    static void Main()
+    {
+        Employee2 e = new Employee2();
+
+        // The data members are inaccessible (private), so
+        // they can't be accessed like this:
+        //    string n = e.name;
+        //    double s = e.salary;
+
+        // 'name' is indirectly accessed via method:
+        string n = e.GetName();
+
+        // 'salary' is indirectly accessed via property
+        double s = e.Salary;
     }
 }
---
+++ Drill_C-_Part22/ErrorExcemption.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace LoggingErrors
+{
+    
+    // Log and exception
+	public static void LogExceptions(Exception exc, string source)
+	{
+        string logFile = @"C:\Users\tanvir\Desktop\Folder_B\test.txt";
+        logFile = httContext.Current.Server.Mypath(logFile);
+
+        // Open the log file for append and write the log
+        StreamWriter sw = new StreamWriter(logFile, true);
+        sw.WriteLine("********** {0} **********", DateTime.Now);
+
+        sw.Close();
+    }
+}
